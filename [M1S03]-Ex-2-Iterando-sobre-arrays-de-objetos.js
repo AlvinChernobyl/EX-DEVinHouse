086e44fe88be00b2d37481fb34105a31302e27c5
DIOGO PEREIRA DIAS <motoblur.connect@Gmail.com>
2022-07-17 13:32:15 -0300

    EX003.2

---
+++ [M1S03]-Ex-2-Iterando-sobre-arrays-de-objetos.js
@@ -0,0 +1,15 @@
+var listaConvidados = [
+    { nome: "José", sobrenome: "Carlos" },
+    { nome: "Alessandro", sobrenome: "Viana" },
+    { nome: "Paula", sobrenome: "Souza" },
+    { nome: "Cristian", sobrenome: "Schimit" },
+    { nome: "Beatriz", sobrenome: "Viana" },
+    { nome: "Fernanda", sobrenome: "Silveira" },
+    { nome: "Cláudia", sobrenome: "Torres" },
+    { nome: "Augusto", sobrenome: "Cesar" },
+    { nome: "Noemi", sobrenome: "Nakamura" },
+    { nome: "Pedro", sobrenome: "Lobo" },
+  ];
+  listaConvidados.forEach(function(nome, i) {
+    console.log(nome.nome+" "+nome.sobrenome);    
+})
