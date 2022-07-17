13472e6f25eca017dcd3e15b13528a1821bd4460
DIOGO PEREIRA DIAS <motoblur.connect@Gmail.com>
2022-07-17 14:29:06 -0300

    EX003.4

---
+++ [M1S03]Ex4-Projeto-iniciando.js
@@ -0,0 +1,18 @@
+var Convidados1 = [
+    { nome: "José", sobrenome: "Carlos", Setor: "Camarote", Idade: 18 }
+  ];
+var Convidado2 = [
+    { nome: "Maria", sobrenome: "Mathues", Setor: "Camarote", Idade: 20 }
+  ];
+var Convidado3 = [
+    { nome: "João", sobrenome: "Pereira", Setor: "Pista", Idade: 30 }
+  ];
+var Convidado4 = [
+    { nome: "Savio", sobrenome: "Silva", Setor: "Pista", Idade: 25 }
+  ];
+var Convidado5 = [
+    { nome: "Paula", sobrenome: "Soares", Setor: "Arquibancada", Idade: 31 }
+  ];
+var Convidado6 = [
+    { nome: "Pricila", sobrenome: "Dias", Setor: "Arquibancada", Idade: 19 }
+  ];
